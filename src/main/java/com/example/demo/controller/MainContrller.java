b1bfc2aa74411860b1985ef03ae571a1e7b74201
sukaikai <1638364772@qq.com>
2020-11-10 11:15:53 +0800

    v0.2 111

--- src/main/java/com/example/demo/controller/MainContrller.java
+++ src/main/java/com/example/demo/controller/MainContrller.java
@@ -1,31 +1,24 @@
 package com.example.demo.controller;
 
-import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/api")
 public class MainContrller {
-	
-    @Autowired
-    private IAppointmentEsEntityRepository appointmentEsEntityRepository;
-    
-	
+
+	@Autowired
+	private IAppointmentEsEntityRepository appointmentEsEntityRepository;
+
 	@GetMapping("/hello")
 	public void hello() {
 		Optional<AppointmentEsEntity> opt = appointmentEsEntityRepository.findById(1l);
-		opt.ifPresent(e->{
+		opt.ifPresent(e -> {
 			System.out.println(e);
 		});
-		
-		SearchQuery query = null;
-		
 		AppointmentEsEntity entity = new AppointmentEsEntity();
 		entity.setId(1l);
 		entity.setAppointmentDate("2020-11-05");
