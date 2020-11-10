0e09a1bb4ecd18f39d524a51c3955158bf067e1b
wangqing <1807540422@qq.com>
2020-11-10 12:02:37 +0800

    v0.5

--- src/main/java/com/example/demo/controller/IAppointmentEsEntityRepository.java
+++ src/main/java/com/example/demo/controller/IAppointmentEsEntityRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+//
 public interface IAppointmentEsEntityRepository extends ElasticsearchRepository<AppointmentEsEntity, Long> {
 
 }
